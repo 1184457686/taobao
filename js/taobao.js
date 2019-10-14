ad03e8bc0c1038b89ca17896e37fd6d63c4a208d
1184457686 <1184457686@qq.com>
2019-10-14 17:49:37 +0800

    updat

--- js/taobao.js
+++ js/taobao.js
@@ -8,6 +8,7 @@
     var search = document.getElementById("submit");
     var option = document.getElementsByClassName("option");
     var  n=search.style.background;
+    var turn = document.getElementById("trun");
 
     option[0].onclick = function  m(x){
         search.style.background = n;
@@ -20,3 +21,35 @@
     option[2].onclick = function  m(x){
         search.style.background = n;
     }
+    var Text= new Array("云","栖","大","会","玩","转","黑","科","技");
+    let i=0;
+    let m= Text[0];
+    //网页跳转
+    turn.onclick =function(){
+        window.open("https://www.baidu.com/","_blank");
+    }
+     
+    setInterval(
+       
+        function(){
+            
+           if(i>=Text.length){
+               i=0;
+               m= Text[0];
+               turn.innerText = m;
+           }else if(i<5){
+            turn.innerText = m;
+           }
+           if(i==4){
+               m =Text[i];
+           }
+           if(i>=4 &&i<=Text.length-1){
+            turn.innerText = m;
+            // i++;
+            // m = m+Text[i];
+           }
+            i=i+1;
+            m = m+Text[i];
+        },500)
+      
+
